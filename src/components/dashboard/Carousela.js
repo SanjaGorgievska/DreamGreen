01a0afd1421681a82e27f97883dc0cfc24e40332
Natasha <natashastojanova6@gmail.com>
2019-10-31 13:22:09 +0100

    Fix SignIn

--- src/components/dashboard/Carousela.js
+++ src/components/dashboard/Carousela.js
@@ -19,18 +19,20 @@ class Carousela extends React.Component {
             
             <div style={mystyle}>
                 
-                <Carousel autoPlay showThumbs={false} className="carouslePhoto">
+    <Carousel autoPlay showThumbs={false} className="carouslePhoto">
+        
+        
       <div style={{ height: '70vh' }}>
-        <img style={{ height: '100%', objectFit: 'cover' }} src="/zena.jpeg" />
-        <p className="legend">Legend 1</p>
+        <img style={{ height: '100%', objectFit: 'cover' }} src="https://wallpaperaccess.com/full/1543715.jpg" />
+       
       </div>
       <div style={{ height: '70vh' }}>
-        <img style={{ height: '100%', objectFit: 'cover' }} src="/maz.jpeg" />
-        <p className="legend">Legend 2</p>
+        <img style={{ height: '100%', objectFit: 'cover' }} src="https://images.pexels.com/photos/56875/tree-dawn-nature-bucovina-56875.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+        
       </div>
       <div style={{ height: '70vh' }}>
-        <img style={{ height: '100%', objectFit: 'cover' }} src="/zena2.jpeg" />
-        <p className="legend">Legend 3</p>
+        <img style={{ height: '100%', objectFit: 'cover' }} src="https://images.pexels.com/photos/38136/pexels-photo-38136.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+       
       </div>
       
     </Carousel>
--- src/components/dashboard/Footer.js
+++ src/components/dashboard/Footer.js
@@ -4,30 +4,34 @@ import Notifications from './Notifications'
 import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
-import { Redirect } from 'react-router-dom'
+import { signOut } from '../../store/actions/authActions'
+import { NavLink } from 'react-router-dom'
 
 
 class Footer extends Component {
     render() {
         return (
             <div className="footer">
-               <div class="container">
+               <div class="container" id = "contactFoo">
                     <div class="row" float="right">
                     <div class="col" id="img1" >
-                    <a href="#"><img src="/contact3.png" width="110px" height="100px"/></a>
-                         <p><b>Поврзетесе со нас</b></p>
-                         <p>Слободно контактирајте не доколку имате<br/>
-                            потреба од дополнителни инфорамции.
-                         </p>
+                    <a href="#"><img src="/contact3.png" width="110px" height="100px"/>
+                    <br/>
+                    <NavLink to='/zaNas' id="kreiraj_proekt"><b>Што правиме ние?</b></NavLink></a>
+                   
+                         
+                         <p>Активирај се за зачувување на <br/>
+                         животната средина и добиј попусти!</p>
                         </div>
                     <div class="col" id="img2">
-                    <a href="#"><img src="/contact2.png" width="110px" height="100px" /></a>
-                         <p><b>Каде да не најдете?</b></p>
-                         <p>Лозирани сме во Скопје, Македонија</p>
+                    <a href="#"><img src="/contact2.png" width="110px" height="100px"/>
+                    <br/>
+                    <NavLink to='/signup' id="kreiraj_proekt"><b>Активирај се и ти!</b></NavLink></a>
+                         <p>Стани дел од групата корисници,<br/>кои се грижат за зелената површина!</p>
                         </div>
                         <div class="col-md-4" id="img3">
                          <a href="#"><img src="/contact1.png" width="110px" height="100px" /></a>
-                         <p><b>Што правиме ние?</b></p>
+                         <p><b>Не лутај,прашај!</b></p>
                          <p>Помагаме да се засадат што<br/>
                               повеќе дрва како мене</p>
                         </div>
